3e2ac0db22569a850b6b31b0fe297f28cd87a0b1
Guilherme <guilhermesevero100@gmail.com>
2026-09-03 15:32:24 -0300

    Adicionando ambiente

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex h-3 w-5 flex-1 bg-amber-50"></div>
+    </>
+  );
+}
+
+export default App;

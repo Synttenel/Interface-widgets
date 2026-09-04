16247d70696adf1f9fc699a3d76161e759b7fb68
Guilherme <guilhermesevero100@gmail.com>
2026-09-03 22:49:21 -0300

    todo: Menu principal

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss, babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 });

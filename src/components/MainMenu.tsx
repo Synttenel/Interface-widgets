002fe21ece06bbc76ef98b954b1db3f99710fd04
Guilherme <guilhermesevero100@gmail.com>
2026-09-10 16:57:26 -0300

    fix:Grid cards fixed

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -18,15 +18,15 @@ function MainMenu({widgets}: Props){
           <h3 className="text-xl">Made by Guilherme Severo</h3>
         </div>
 
-        <div className="grid grid-flow-col  justify-start items-center  w-screen  bg-gray-800/70 p-5  rounded-2xl gap-10 overflow-x-auto snap-x snap-mandatory md:grid-flow-row md:justify-center">
+        <div className="w-screen grid grid-flow-col overflow-x-auto snap-x snap-mandatory auto-cols-[100%] p-3 gap-2 justify-start">
           {widgets.map((widget, _) => (
-            <div className="flex flex-col justify-center items-center  bg-blue-600/80 rounded-2xl p-2 w-75 h-100 gap-1 snap-center"
+            <div className="flex flex-col justify-center items-center  bg-blue-600/80 rounded-2xl p-2 w-full h-auto gap-1 snap-center"
             key={widget.id}>
-            <img className="w-full h-100 bg-no-repeat bg-cover rounded-t-4xl rounded-b-xl bg-center"
+            <img className="w-full  bg-no-repeat bg-cover rounded-t-4xl rounded-b-xl bg-center"
             src={widget.image}/>
-            <div className="size-[80%] text-3xl font-bold text-left tracking-tighter">{widget.title}</div>
-            <div className="size-[80%] text-2xl  text-left tracking-wide">{widget.description}</div>
-            <button className="rounded-2xl size-30 bg-black p-2 font-bold hover:rounded-3xl hover: hover:italic transition-all hover:tracking-widest"
+            <div className=" text-3xl font-bold text-left tracking-tighter">{widget.title}</div>
+            <div className=" text-2xl  text-left tracking-wide">{widget.description}</div>
+            <button className="rounded-2xl w-[90%] h-10 mt-10 bg-black p-2 font-bold hover:rounded-3xl hover: hover:italic transition-all hover:tracking-widest"
             onClick={() => {widget.redirect()}}>Acessar</button>
             
           </div>
